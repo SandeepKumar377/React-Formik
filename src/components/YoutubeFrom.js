495b0a88693197084ac63959e569b46d87fe4596
Sandeep Kumar <sandeepkumar.sk5.ss55@gmail.com>
2021-11-20 17:08:54 +0530

    Schema validation with Yup

--- src/components/YoutubeFrom.js
+++ src/components/YoutubeFrom.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useFormik } from 'formik'
+import * as Yup from 'yup'
 
 const initialValues = {
     name: '',
@@ -32,12 +33,20 @@ const validate = (values) => {
     return errors
 }
 
+const validationSchema = Yup.object({
+    name: Yup.string().required('Required!'),
+    email: Yup.string().email('Invalid email!').required('Required!'),
+    channel: Yup.string().required('Required!'),
+
+})
+
 const YoutubeFrom = () => {
 
     const formik = useFormik({
         initialValues,
         onSubmit,
-        validate
+        validationSchema
+        // validate
     })
     console.log('Visited Field : ', formik.touched)
 
